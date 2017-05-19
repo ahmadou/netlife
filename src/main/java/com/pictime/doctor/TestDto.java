66d450a39499e6be67e6a38975f4605983a2e7ce
ahmadou <ahmadou.diouf@gmail.com>
2017-05-20 01:14:46 +0200

    greeting test

---
+++ src/main/java/com/pictime/doctor/TestDto.java
@@ -0,0 +1,16 @@
+package com.pictime.doctor;
+
+/**
+ * Created by pictime on 20/05/17.
+ */
+public class TestDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
